2c4e9700a166d657946f5303eacf5a65a753a319
gzile <gocezile@gmail.com>
2022-09-28 20:15:33 +0200

    Update starter app

--- src/index.js
+++ src/index.js
@@ -2,37 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import avatarImage from './images/avatar.png'
-
-const hexaColor = () => {
-  let str = '0123456789abcdef'
-  let color = ''
-  for (let i = 0; i < 6; i++) {
-    let index = Math.floor(Math.random() * str.length)
-    color += str[index]
-  }
-  return '#' + color
-}
-
-const HexaColor = () => {
-  const bgColor = hexaColor()
-  const styles = {
-    height: '100px',
-    display: 'flex',
-    'justify-content': 'center',
-    'align-items': 'center',
-    fontFamily: 'Montserrat',
-    margin: '2px auto',
-    borderRadius: '5px',
-    width: '75%',
-    border: '2px solid black',
-    background: bgColor,
-  }
-  return (
-    <div style={styles}>
-      <h2>{bgColor}</h2>
-    </div>
-  )
-}
+import './styles/Style.css'
 
 // Header Component
 const Header = () => (
